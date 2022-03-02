686ab1285f252a918953177f8a4f9d19ce58c736
Ajay Maurya <97455068+Ajay-Maury@users.noreply.github.com>
2022-03-02 23:00:32 +0530

    Day 1 Assignment Done

--- Day 1/main.js
+++ Day 1/main.js
@@ -1,11 +1,14 @@
 const add = require("./add")
 console.log(add(10,2))
 
+
 const substract = require("./subtract")
 console.log(substract(10,2))
 
+
 const multiply = require("./multiply")
 console.log(multiply(10,2))
+
 
 const divide = require("./divide")
 console.log(divide(10,2))
